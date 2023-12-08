e1f13744931942a4bfe920bd1873798270a798ed
guilherme.ribeiro <guilhermeribeirog70@gmail.com>
2023-12-08 09:28:34 -0400

    Realizada atualização com a inclusão: - API do OMDb para consumir filmes; - inclusão do pacote GSON; - representação de dados através do record; - tratamento de erro e exceções; - criação de lista para filmes favoritos.

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,8 +1,12 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Titulo implements Comparable<Titulo> {
+import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoException;
+import com.google.gson.annotations.SerializedName;
 
+public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -12,6 +16,15 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        if (meuTituloOmdb.year().length() > 4){
+            throw  new ErroDeConversaoDeAnoException("Não consegui converter o ano porque tem mais de 04 caracteres.");
+        }
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 3));
     }
 
     public String getNome() {
@@ -67,5 +80,10 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "(nome = " + nome + ", anoDeLancamento = " + anoDeLancamento + "," + "duração = " + duracaoEmMinutos + ")";
     }
 }
